87afae43f7269236f38498b967dd7aceb7376154
Jordi <55429631+jorbush@users.noreply.github.com>
2022-10-20 21:58:40 +0200

    title

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      {/* <h1>Silvia Valiente Vázquez</h1> */}
+      <h2>Silvia's Mural</h2>
+      {/* <p>Welcome to your Image Gallery</p> */}
+    </div>
+  );
+};
+
+export default Title;
